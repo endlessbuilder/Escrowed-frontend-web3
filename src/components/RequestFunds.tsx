3be794f2eb1c34270c43d84d022a1b430b126b36
endlessbuilder <continuebuilding@gmail.com>
2024-10-11 06:32:28 -0400

    update: contract page with deed id, filter on homepage

--- src/components/RequestFunds.tsx
+++ src/components/RequestFunds.tsx
@@ -1,33 +1,33 @@
-"use client";
+'use client';
 
-import { Button } from "@/components/ui/button";
-import React, { useRef, useState } from "react";
-import Milestones from "./Milestones";
-import { Upload, XIcon } from "lucide-react";
+import { Button } from '@/components/ui/button';
+import React, { useRef, useState } from 'react';
+import Milestones from './Milestones';
+import { Upload, XIcon } from 'lucide-react';
 
 import web3 from '@/utils/web3';
 import escrowed from '@/utils/escrowed';
 
 function RequestFunds() {
-  const [projectLink, setProjectLink] = useState("");
+  const [projectLink, setProjectLink] = useState('');
   const [mileStonesObj, setMileStonesObj] = useState([
     {
       id: 0,
-      milestone: "Userflow & designing",
-      amount: "1200",
-      expectedTime: "",
+      milestone: 'Userflow & designing',
+      amount: '1200',
+      expectedTime: '',
     },
     {
       id: 1,
-      milestone: "Frontend Development",
-      amount: "1200",
-      expectedTime: "",
+      milestone: 'Frontend Development',
+      amount: '1200',
+      expectedTime: '',
     },
     {
       id: 2,
-      milestone: "Backend development & smart contract",
-      amount: "1200",
-      expectedTime: "",
+      milestone: 'Backend development & smart contract',
+      amount: '1200',
+      expectedTime: '',
     },
   ]);
   const mileStones = true;
@@ -48,24 +48,24 @@ function RequestFunds() {
       console.error('Error markMilestoneAsDoneBySupplier:', error);
       alert('Failed to markMilestoneAsDoneBySupplier');
     }
-  }
+  };
 
   return (
-    <div className="h-screen w-screen flex flex-col items-start justify-start bg-[#F8F8F8] p-4 overflow-x-hidden gap-y-8">
-      <div className="flex flex-col justify-center gap-y-2">
+    <div className='flex h-screen w-screen flex-col items-start justify-start gap-y-8 overflow-x-hidden bg-[#F8F8F8] p-4'>
+      <div className='flex flex-col justify-center gap-y-2'>
         {/* Header */}
-        <header className="text-[#484848] text-lg font-bold">
+        <header className='text-lg font-bold text-[#484848]'>
           <h1>Request Funds</h1>
         </header>
       </div>
-      <div className="flex flex-col gap-y-3 w-full">
+      <div className='flex w-full flex-col gap-y-3'>
         <div>
-          <h1 className="text-[#484848] font-bold">Request Funds for</h1>
+          <h1 className='font-bold text-[#484848]'>Request Funds for</h1>
         </div>
         {!mileStones && (
-          <div className="w-full flex flex-col gap-y-2">
+          <div className='flex w-full flex-col gap-y-2'>
             <div
-              className="w-full flex justify-between text-[#5D5D5D]"
+              className='flex w-full justify-between text-[#5D5D5D]'
               onClick={() => {
                 if (radioRef?.current) {
                   if (radioRef?.current?.checked) {
@@ -76,28 +76,30 @@ function RequestFunds() {
                 }
               }}
             >
-              <div className="flex gap-1 items-center justify-center">
+              <div className='flex items-center justify-center gap-1'>
                 <input
-                  type="radio"
-                  className="border-[#52B9FF] border-2 outline-none bg-[#52B9FF]"
+                  type='radio'
+                  className='border-2 border-[#52B9FF] bg-[#52B9FF] outline-none'
                   ref={radioRef}
                 />
                 <label>Complete Project</label>
               </div>
-              <p className="text-[#52B9FF]">$1200</p>
+              <p className='text-[#52B9FF]'>$1200</p>
             </div>
           </div>
         )}
         {mileStones &&
-          mileStonesObj.map((val) => {
+          mileStonesObj.map((val, index) => {
             return (
-              <>
-                <Milestones milestone={val?.milestone} amount={val?.amount} />
-              </>
+              <Milestones
+                milestone={val?.milestone}
+                amount={val?.amount}
+                key={'mileStonesObj' + index}
+              />
             );
           })}
       </div>
-      <div className="flex flex-col gap-y-2 w-full">
+      <div className='flex w-full flex-col gap-y-2'>
         <div>
           <h1>Proof of work (Optional)</h1>
         </div>
@@ -107,77 +109,80 @@ function RequestFunds() {
             very helpful in case of any fraud.
           </p>
         </div>
-        <div className="flex flex-col justify-center items-center w-full gap-y-2">
-          <div className="flex p-2 justify-center items-center w-full bg-[#FFFFFF] border-[#C4C4C4] border-2 rounded-md">
+        <div className='flex w-full flex-col items-center justify-center gap-y-2'>
+          <div className='flex w-full items-center justify-center rounded-md border-2 border-[#C4C4C4] bg-[#FFFFFF] p-2'>
             <input
-              type="text"
-              className="rounded-md outline-none w-full"
-              placeholder="Project Link"
+              type='text'
+              className='w-full rounded-md outline-none'
+              placeholder='Project Link'
               onChange={(e) => setProjectLink(e.target.value)}
               value={projectLink}
             />
             <div
               onClick={() => {
-                setProjectLink("");
+                setProjectLink('');
               }}
             >
-              <XIcon className="w-5 h-5 text-[#5D5D5D]" />
+              <XIcon className='h-5 w-5 text-[#5D5D5D]' />
             </div>
           </div>
-          <p className="text-[#C4C4C4]">Or</p>
+          <p className='text-[#C4C4C4]'>Or</p>
           <div
-            className="min-h-[120px] flex flex-col gap-y-2 justify-center items-center text-[#C4C4C4] border-[#C4C4C4] border-2 rounded-md w-full"
+            className='flex min-h-[120px] w-full flex-col items-center justify-center gap-y-2 rounded-md border-2 border-[#C4C4C4] text-[#C4C4C4]'
             onClick={() => {
               fileRef?.current?.click();
             }}
           >
             <Upload />
             <p>Select a file to upload</p>
-            <input type="file" ref={fileRef} className="hidden" />
+            <input type='file' ref={fileRef} className='hidden' />
           </div>
         </div>
       </div>
-      <div className="flex flex-col gap-y-3 w-full">
+      <div className='flex w-full flex-col gap-y-3'>
         {/* Deed Details */}
         <div>
-          <h1 className="text-[#484848] font-bold">Deed Info Summary</h1>
+          <h1 className='font-bold text-[#484848]'>Deed Info Summary</h1>
         </div>
-        <div className="w-full flex flex-col gap-y-2">
-          <div className="w-full flex justify-between text-[#5D5D5D]">
+        <div className='flex w-full flex-col gap-y-2'>
+          <div className='flex w-full justify-between text-[#5D5D5D]'>
             <p>Deed Buyer</p>
-            <p className="text-[#52B9FF]">You(34 Plebi)</p>
+            <p className='text-[#52B9FF]'>You(34 Plebi)</p>
           </div>
         </div>
-        <div className="p-[0.5px] bg-[#EDEDED]"></div>
+        <div className='bg-[#EDEDED] p-[0.5px]'></div>
 
-        <div className="w-full flex flex-col gap-y-2">
-          <div className="w-full flex justify-between text-[#5D5D5D]">
+        <div className='flex w-full flex-col gap-y-2'>
+          <div className='flex w-full justify-between text-[#5D5D5D]'>
             <p>Payment Distribution Type</p>
-            <p className="text-[#52B9FF]">One Time</p>
+            <p className='text-[#52B9FF]'>One Time</p>
           </div>
         </div>
-        <div className="p-[0.5px] bg-[#EDEDED]"></div>
+        <div className='bg-[#EDEDED] p-[0.5px]'></div>
 
-        <div className="w-full flex flex-col gap-y-2">
-          <div className="w-full flex justify-between text-[#5D5D5D]">
+        <div className='flex w-full flex-col gap-y-2'>
+          <div className='flex w-full justify-between text-[#5D5D5D]'>
             <p>Deed Completion Time</p>
-            <p className="text-[#52B9FF]">4.5 weeks</p>
+            <p className='text-[#52B9FF]'>4.5 weeks</p>
           </div>
         </div>
-        <div className="p-[0.5px] bg-[#EDEDED]"></div>
+        <div className='bg-[#EDEDED] p-[0.5px]'></div>
 
-        <div className="w-full flex flex-col gap-y-2">
-          <div className="w-full flex justify-between text-[#5D5D5D]">
+        <div className='flex w-full flex-col gap-y-2'>
+          <div className='flex w-full justify-between text-[#5D5D5D]'>
             <p>Total Deed Amount</p>
-            <p className="text-[#52B9FF]">$1200</p>
+            <p className='text-[#52B9FF]'>$1200</p>
           </div>
         </div>
-        <div className="p-[0.5px] bg-[#EDEDED]"></div>
+        <div className='bg-[#EDEDED] p-[0.5px]'></div>
       </div>
-      <div className="w-full flex gap-2 min-h-[120px]">
+      <div className='flex min-h-[120px] w-full gap-2'>
         {/* Share */}
-        <div className="w-full">
-          <Button className="w-full bg-[#52B9FF] flex justify-center items-center gap-2" onClick={()=>onClickRequestFunds()}>
+        <div className='w-full'>
+          <Button
+            className='flex w-full items-center justify-center gap-2 bg-[#52B9FF]'
+            onClick={() => onClickRequestFunds()}
+          >
             Request Funds
           </Button>
         </div>
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,28 @@
-export const USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
-export const SEPOLIA_RPC = "https://rpc.ankr.com/eth_sepolia/3becd2d01b34b9aaada14a2aad12f01089cebabed27ea5bf1c950c413b34430f"
-export const CONTRACT_ADDRESS = "0x223a770d7584D01725c91dfC636eD2866BdC0f14"
+export const USDC = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
+export const SEPOLIA_RPC =
+  'https://rpc.ankr.com/eth_sepolia/3becd2d01b34b9aaada14a2aad12f01089cebabed27ea5bf1c950c413b34430f';
+export const CONTRACT_ADDRESS = '0x223a770d7584D01725c91dfC636eD2866BdC0f14';
+
+export const SERVER_IP = 'http://localhost:5000';
+
+export interface Deed {
+  id: number;
+  title: string;
+  description: string;
+  payment_method: any;
+  payment_type: any;
+  amount: number;
+  timeline: any;
+  status: any;
+  buyer_id: number;
+  seller_id: number;
+  category: any;
+  createdAt: any;
+}
+
+export interface User {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+}
